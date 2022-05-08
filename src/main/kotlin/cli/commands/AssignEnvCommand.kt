311e9977fc79e21f882e68099511c134c8fec514
vladkash <spanch-kash@yandex.ru>
2022-05-08 16:21:47 +0500

    fixes hw1

--- src/main/kotlin/cli/commands/AssignEnvCommand.kt
+++
@@ -1,9 +0,0 @@
-package cli.commands
-
-import cli.environments.MutableEnv
-
-class AssignEnvCommand(private val key: String, private val value: String) : Command {
-    override fun modifyEnv(env: MutableEnv) {
-        env.put(key, value)
-    }
-}
