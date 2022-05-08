2d6a3f7966dc7f25b3f00687b5265996ecccbc13
vladkash <spanch-kash@yandex.ru>
2022-05-08 16:28:53 +0500

    fixes hw1

--- src/test/kotlin/cli/TestWithFakerAndEnv.kt
+++ src/test/kotlin/cli/TestWithFakerAndEnv.kt
@@ -1,6 +1,5 @@
 package cli
 
-import cli.environments.MutableEnv
 import io.github.serpro69.kfaker.Faker
 
 abstract class TestWithFakerAndEnv {
@@ -9,5 +8,5 @@ abstract class TestWithFakerAndEnv {
 
     protected val envKey = faker.random.randomString()
 
-    protected val env = MutableEnv(mutableMapOf(envKey to faker.random.randomString()))
+    protected val env = mutableMapOf(envKey to faker.random.randomString())
 }
